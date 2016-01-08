7f403445fd382a4774dadcdc239ce6cc000376da
Greg Zoller <gwzoller@yahoo.com>
2016-01-08 13:13:01 -0600

    Revert "Merge pull request #35 from gzoller/feature/asyncmongo"
    This reverts commit d156f985e068595a56b35eaadaee22e167a87e4d, reversing
    changes made to dc821a358de61dcd7e07b5373c0320b84a2e2094.

--- mongo/src/main/scala/co.blocke.scalajack/Mongo.scala
+++ mongo/src/main/scala/co.blocke.scalajack/Mongo.scala
@@ -2,34 +2,32 @@ package co.blocke.scalajack
 
 import scala.collection.mutable.LinkedHashMap
 import scala.language.implicitConversions
-import org.mongodb.scala._
-import org.mongodb.scala.bson.collection.immutable.Document
-import org.mongodb.scala.bson.BsonObjectId
+import com.mongodb.casbah.Imports._
 
 package object mongo {
-	implicit def mongoOID( s:String ) = BsonObjectId( s)
+	implicit def mongoOID( s:String ) = new ObjectId( s)
 
-	val OBJECT_ID = "org.bson.BsonObjectId"
+	val OBJECT_ID = "org.bson.types.ObjectId"
 }
 import mongo._
 
 // Custom type
 case class ObjectIdType(name:String) extends CustomType {
 	val renderers = Map(  // ObjectId -> <something>
-		"default" -> ((a:Any) => "\""+a.asInstanceOf[BsonObjectId].getValue.toString+"\""),
+		"default" -> ((a:Any) => "\""+a+"\""),
 		"mongo"   -> ((a:Any) => a)
 		)
 	val readers   = Map(
-		"default" -> ((a:Any) => BsonObjectId(a.toString)),
+		"default" -> ((a:Any) => new ObjectId(a.toString)),
 		"mongo"   -> ((a:Any) => a)
 		)
 	def dup = this.copy()
 }
 
-case class ScalaJack_Mongo() extends ScalaJack[Document] with mongo.MongoReadRenderFrame {
+case class ScalaJack_Mongo() extends ScalaJack[MongoDBObject] with mongo.MongoReadRenderFrame {
 	Analyzer.addType(OBJECT_ID,ObjectIdType(OBJECT_ID))
 } 
 
-case class MongoType() extends SupportedType[Document] {
-	def makeScalaJack():ScalaJack[Document] = ScalaJack_Mongo()
+case class MongoType() extends SupportedType[MongoDBObject] {
+	def makeScalaJack():ScalaJack[MongoDBObject] = ScalaJack_Mongo()
 }
