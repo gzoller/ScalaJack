da2da92d7758505014626dae3c14c38b4056ab22
Adam Paynter <adam.paynter@capitalone.com>
2016-09-25 07:48:47 -0500

    Initial attempt at tokenizer-based non-canonical key support.

---
+++ core/src/main/scala/co.blocke.scalajack/typeadapter/StringParsingTypeAdapter.scala
@@ -0,0 +1,24 @@
+package co.blocke.scalajack.typeadapter
+
+import co.blocke.scalajack.json.{StringJsonWriter, Tokenizer}
+import co.blocke.scalajack.{Reader, TypeAdapter, Writer}
+
+case class StringParsingTypeAdapter[T](tokenizer: Tokenizer,
+                                       stringTypeAdapter: TypeAdapter[String],
+                                       valueTypeAdapter: TypeAdapter[T]) extends TypeAdapter[T] {
+
+  override def read(reader: Reader): T = {
+    val json = stringTypeAdapter.read(reader)
+    val jsonCharArray = json.toCharArray
+    val nestedReader = tokenizer.tokenize(jsonCharArray, 0, jsonCharArray.length)
+    valueTypeAdapter.read(nestedReader)
+  }
+
+  override def write(value: T, writer: Writer): Unit = {
+    val nestedWriter = new StringJsonWriter(canonical = false)
+    valueTypeAdapter.write(value, nestedWriter)
+    val json = nestedWriter.jsonString
+    stringTypeAdapter.write(json, writer)
+  }
+
+}
