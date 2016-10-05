5acb4a7e3045f869296de68f4e53408ae7ac7741
Greg Zoller <gwzoller@yahoo.com>
2016-10-05 08:48:15 -0500

    Add trait tests

--- core/src/test/scala/co.blocke.scalajack/test.parameters/Model.scala
+++ core/src/test/scala/co.blocke.scalajack/test.parameters/Model.scala
@@ -21,3 +21,37 @@ case class Bar4[X, Y, Z](id: X, thing1: Z, thing2: Y)
 case class Blah[T, U](t: T, u: U)
 case class Foo7[A, B, C, D](x: Bar5[C, D, A], y: B)
 case class Bar5[X, Y, Z](id: Y, blah: Blah[Z, X])
+
+//--- Basic Parameterized Trait
+trait T1[X] { val x: X }
+case class TFoo1[A](x: A, b: Int) extends T1[A]
+trait T2 { val name: String }
+case class TBar1(name: String) extends T2
+
+//--- Advanced Parameterized Trait
+trait T3[X] { val thing: X }
+case class TBar2(thing: Boolean) extends T3[Boolean]
+case class TBar3[T](thing: T) extends T3[T]
+trait T4[X] { val x: TBar3[X] }
+case class TFoo2[A](x: TBar3[A], b: A) extends T4[A]
+trait T5[X, Y] { val thing1: X; val thing2: Y }
+case class TBar4[T](thing1: T, thing2: String) extends T5[T, String]
+trait T6[X] { val x: List[T5[X, String]] }
+case class TFoo3[A](x: List[T5[A, String]]) extends T6[A]
+
+//--- Very Advanced Parameterized Trait
+trait T7[X, Y] { val x: T5[X, Y]; val b: X }
+case class TBar5[T, U](thing1: T, thing2: U) extends T5[T, U]
+case class TFoo4[A, B](x: T5[A, B], b: A) extends T7[A, B]
+
+trait T8[W, X, Y, Z] { val x: T9[Y, Z, W]; val y: X }
+trait T9[T, U, V] { val pi: T; val po: U; val pu: V }
+case class TBar6[A, B, C](pi: A, po: B, pu: C) extends T9[A, B, C]
+case class TFoo5[A, B, C, D](x: T9[C, D, A], y: B) extends T8[A, B, C, D]
+
+// Foo[A,B,C,D](x:Bar[C,Blah[D,A]], y:B)
+trait T10[X, Y] { val x: X; val y: Y }
+trait T11[W, Z] { val w: W; val z: Z }
+case class TBlah1[A, B](w: A, z: B) extends T11[A, B]
+case class TBar7[A, B](thing1: A, thing2: B) extends T5[A, B]
+case class TFoo6[A, B, C, D](x: T11[C, T5[D, A]], y: B) extends T10[T11[C, T5[D, A]], B]
