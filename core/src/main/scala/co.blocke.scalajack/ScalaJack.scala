0c70308837332323b18534fb071358d851ec37f4
Greg Zoller <gwzoller@yahoo.com>
2016-10-07 17:00:12 -0500

    Move tests into json directory

--- core/src/main/scala/co.blocke.scalajack/ScalaJack.scala
+++ core/src/main/scala/co.blocke.scalajack/ScalaJack.scala
@@ -9,46 +9,23 @@ import scala.language.existentials
 import scala.reflect.runtime.universe.{ Type, TypeTag }
 
 object ScalaJack {
-  def apply[S](kind: JackFlavor[S] with ScalaJackLike[S] = JsonFlavor()): JackFlavor[S] with ScalaJackLike[S] = kind
+  def apply[S](kind: ScalaJackLike[S] = JsonFlavor()): ScalaJackLike[S] = kind
 }
 
-trait ScalaJackLike[S] {
-  self: JackFlavor[S] => // require a JackFlavor to be mixed in
+abstract class ScalaJackLike[S] extends JackFlavor[S] {
+  val customAdapters: List[TypeAdapterFactory]
+  val hintMap: Map[Type, String]
+  val hintModifiers: Map[Type, HintModifier]
+  val parseOrElseMap: Map[Type, Type]
+  val defaultHint: String
 
-  private var customAdapters = List.empty[TypeAdapterFactory]
-  private var hintMap = Map.empty[Type, String]
-  private var hintModifiers = Map.empty[Type, HintModifier]
-  private var parseOrElseMap = Map.empty[Type, Type]
-  private var defaultHint = "_hint"
-  private var _context: Context = bakeContext()
+  val context: Context = bakeContext()
 
-  def context = _context
-
-  private def rebuild() = {
-    _context = bakeContext()
-    this
-  }
-
-  def withAdapters(ta: TypeAdapterFactory*) = {
-    customAdapters = customAdapters ++ ta.toList
-    rebuild()
-  }
-  def withHints(h: (Type, String)*) = {
-    hintMap = hintMap ++ h
-    rebuild()
-  }
-  def withHintModifiers(hm: (Type, HintModifier)*) = {
-    hintModifiers = hintModifiers ++ hm
-    rebuild()
-  }
-  def withDefaultHint(hint: String) = {
-    defaultHint = hint
-    rebuild()
-  }
-  def parseOrElse(poe: (Type, Type)*) = {
-    parseOrElseMap = parseOrElseMap ++ poe
-    rebuild()
-  }
+  def withAdapters(ta: TypeAdapterFactory*): ScalaJackLike[S]
+  def withHints(h: (Type, String)*): ScalaJackLike[S]
+  def withHintModifiers(hm: (Type, HintModifier)*): ScalaJackLike[S]
+  def withDefaultHint(hint: String): ScalaJackLike[S]
+  def parseOrElse(poe: (Type, Type)*): ScalaJackLike[S]
 
   /**
    * Project fields from given master object to a view object of type T.  Field names/types must match master
@@ -144,14 +121,6 @@ trait ScalaJackLike[S] {
       factories = fallbackFactories ::: intermediateContext.factories
     )
   }
-
 }
 
 case class ViewException(msg: String) extends Exception(msg)
-
-//-----------------------------------------------------------
-
-// MOVE TO UTIL CLASS OR SOMETHING!
-// object JSON {
-// 	def toCollection( js:String, size:Int = 500 ) : Either[Map[String,Any],List[Any]] = json.FastTokenizer(size).tokenize(js.toCharArray).toCollection()
-// }
