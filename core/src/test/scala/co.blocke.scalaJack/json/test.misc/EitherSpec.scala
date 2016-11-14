c0e7661cb76608fad06c9f7b905cce65c3a5eaac
Greg Zoller <gwzoller@yahoo.com>
2016-11-14 15:05:41 -0600

    Made type handling more specific rather than Any

--- core/src/test/scala/co.blocke.scalaJack/json/test.misc/EitherSpec.scala
+++ core/src/test/scala/co.blocke.scalaJack/json/test.misc/EitherSpec.scala
@@ -10,11 +10,8 @@ case class DumpTruck(axles: Int)
 
 case class EitherHolder[L, R](either: Either[L, R])
 
-object MyTypes {
-  type Phone = String
-  type Size = String
-}
-import MyTypes._
+case class Chair(numLegs: Int)
+case class Table(numLegs: Int)
 
 class EitherSpec extends FunSpec with GivenWhenThen with BeforeAndAfterAll {
 
@@ -24,7 +21,7 @@ class EitherSpec extends FunSpec with GivenWhenThen with BeforeAndAfterAll {
     it("Left - two class types") {
       val inst: Either[Parrot, DumpTruck] = Left(Parrot("blue"))
       val js = sj.render(inst)
-      assertResult("""{"_hint":"co.blocke.scalajack.json.test.misc.Parrot","color":"blue"}""") { js }
+      assertResult("""{"color":"blue"}""") { js }
       assertResult(inst) {
         sj.read[Either[Parrot, DumpTruck]](js)
       }
@@ -32,7 +29,7 @@ class EitherSpec extends FunSpec with GivenWhenThen with BeforeAndAfterAll {
     it("Right - two class types") {
       val inst: Either[Parrot, DumpTruck] = Right(DumpTruck(axles = 2))
       val js = sj.render(inst)
-      assertResult("""{"_hint":"co.blocke.scalajack.json.test.misc.DumpTruck","axles":2}""") { js }
+      assertResult("""{"axles":2}""") { js }
       assertResult(inst) {
         sj.read[Either[Parrot, DumpTruck]](js)
       }
@@ -40,7 +37,7 @@ class EitherSpec extends FunSpec with GivenWhenThen with BeforeAndAfterAll {
     it("Left - class type and scalar type") {
       val inst: Either[Parrot, String] = Left(Parrot("red"))
       val js = sj.render(inst)
-      assertResult("""{"_hint":"co.blocke.scalajack.json.test.misc.Parrot","color":"red"}""") { js }
+      assertResult("""{"color":"red"}""") { js }
       assertResult(inst) {
         sj.read[Either[Parrot, DumpTruck]](js)
       }
@@ -57,13 +54,40 @@ class EitherSpec extends FunSpec with GivenWhenThen with BeforeAndAfterAll {
       val inst: Either[Parrot, String] = null
       val js = sj.render(inst)
       assertResult("null") { js }
-      assertResult(inst) {
+      assertResult(Right(null)) {
         sj.read[Either[Parrot, String]](js)
       }
     }
     it("Same instance Left and Right") {
       val js = "\"foo\""
       the[java.lang.IllegalArgumentException] thrownBy sj.read[Either[String, String]](js) should have message "Types String and String are not mutually exclusive"
+    }
+    it("Different classes with identical fields--favor Right") {
+      val js = """{"numLegs":4}"""
+      assertResult(sj.read[Either[Chair, Table]](js)) { Right(Table(4)) }
+    }
+    it("Handles traits - Right") {
+      val inst = EitherHolder[String, Pet](Right(Dog("Fido", 13)))
+      val js = sj.render(inst)
+      assertResult("""{"either":{"_hint":"co.blocke.scalajack.json.test.misc.Dog","name":"Fido","kind":13}}""")(js)
+      assertResult(inst) {
+        sj.read[EitherHolder[String, Pet]](js)
+      }
+    }
+    it("Handles traits - Left") {
+      val inst = EitherHolder[Pet, String](Left(Dog("Fido", 13)))
+      val js = sj.render(inst)
+      assertResult("""{"either":{"_hint":"co.blocke.scalajack.json.test.misc.Dog","name":"Fido","kind":13}}""")(js)
+      assertResult(inst) {
+        sj.read[EitherHolder[Pet, String]](js)
+      }
+    }
+    it("Neither value works") {
+      val js = "25"
+      val msg = """Parsed value fits neither class class java.lang.String nor boolean
+        |25
+        |^""".stripMargin
+      the[java.lang.RuntimeException] thrownBy sj.read[Either[String, Boolean]](js) should have message msg
     }
   }
 }
