037cff199c08774e84e4f9c71b901412e7789daf
gzoller <gzoller@gmail.com>
2017-09-01 16:25:10 -0500

    remove logging

--- mongo/src/main/scala/co.blocke.scalajack/mongo/BsonReader.scala
+++ mongo/src/main/scala/co.blocke.scalajack/mongo/BsonReader.scala
@@ -77,12 +77,7 @@ class BsonReader(
   // This guy has to do type inference, as a Number could be: Byte, Double, Float, Integer, Long, or Short.
   override def readNumber(forJava: Boolean): Number = {
     read(expected = TokenType.Number)
-    println("-- Here in readNumber --")
     val value = values(position)
-    println("Value: " + value)
-    println("isDouble: " + value.isDouble)
-    println("isInt32: " + value.isInt32)
-    println("isInt64: " + value.isInt64)
     if (value.isDouble) {
       value.asDouble.doubleValue
     } else if (value.isInt32) {
