ebd846b1128053d1fd64746266f50ebef90f49dc
Adam Paynter <adam.paynter@capitalone.com>
2018-06-09 21:49:33 -0500

    Implemented `deserializer` and `serializer` of `BooleanTypeAdapter`.

--- core/src/main/scala/co.blocke.scalajack/typeadapter/BooleanTypeAdapter.scala
+++ core/src/main/scala/co.blocke.scalajack/typeadapter/BooleanTypeAdapter.scala
@@ -1,7 +1,21 @@
 package co.blocke.scalajack
 package typeadapter
 
+import scala.reflect.runtime.universe.{ Type, typeOf }
+
 object BooleanTypeAdapter extends TypeAdapter.=:=[Boolean] {
+
+  override object deserializer extends Deserializer[Boolean] {
+
+    private val BooleanType: Type = typeOf[Boolean]
+
+    override def deserialize[J](path: Path, json: J)(implicit ops: JsonOps[J]): DeserializationResult[Boolean] =
+      json match {
+        case JsonBoolean(value) => DeserializationSuccess(TypeTagged[Boolean](value, BooleanType))
+        case _                  => DeserializationFailure(path, DeserializationError.Unsupported("Expected a JSON boolean"))
+      }
+
+  }
 
   override def read(reader: Reader): Boolean = {
     reader.peek match {
@@ -21,6 +35,15 @@ object BooleanTypeAdapter extends TypeAdapter.=:=[Boolean] {
         throw new IllegalStateException(s"Expected value token of type True or False, not $actual when reading Boolean value.  (Is your value wrapped in quotes or a number?)\n" + reader.showError())
       }
     }
+  }
+
+  override object serializer extends Serializer[Boolean] {
+
+    override def serialize[J](tagged: TypeTagged[Boolean])(implicit ops: JsonOps[J]): SerializationResult[J] =
+      tagged match {
+        case TypeTagged(value) => SerializationSuccess(JsonBoolean(value))
+      }
+
   }
 
   override def write(value: Boolean, writer: Writer): Unit =
