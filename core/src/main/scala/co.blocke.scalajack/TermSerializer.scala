02eb6c888510dcaa86d7207b36c10cc98ca37e1e
Adam Paynter <adam.paynter@capitalone.com>
2018-06-16 22:24:29 -0500

    Attempting to fix compilation errors.

--- core/src/main/scala/co.blocke.scalajack/TermSerializer.scala
+++ core/src/main/scala/co.blocke.scalajack/TermSerializer.scala
@@ -2,6 +2,7 @@ package co.blocke.scalajack
 
 class TermSerializer[T](next: Serializer[T]) extends Serializer[T] {
 
-  override def serialize[J](tagged: TypeTagged[T])(implicit ops: JsonOps[J]): SerializationResult[J] = ???
+  override def serialize[J](tagged: TypeTagged[T])(implicit ops: JsonOps[J]): SerializationResult[J] =
+    next.serialize(tagged) // TODO implement this method
 
 }
