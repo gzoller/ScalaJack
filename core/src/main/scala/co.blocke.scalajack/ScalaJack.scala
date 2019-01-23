31ac8354d50a1c6e7108056ce4a30fe75bf54bb9
gzoller <gzoller@gmail.com>
2019-01-23 14:28:44 -0600

    clean up a few imports

--- core/src/main/scala/co.blocke.scalajack/ScalaJack.scala
+++ core/src/main/scala/co.blocke.scalajack/ScalaJack.scala
@@ -3,10 +3,6 @@ package co.blocke.scalajack
 import model._
 import json.JsonFlavor
 
-//import co.blocke.scalajack.BijectiveFunctions._
-//import co.blocke.scalajack.json.JsonFlavor
-//import co.blocke.scalajack.typeadapter._
-
 object ScalaJack {
   def apply[N, WIRE](kind: JackFlavor[N, WIRE] = JsonFlavor()): JackFlavor[N, WIRE] = kind
 }
