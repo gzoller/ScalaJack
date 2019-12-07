d7692c37563fefba85e8afc6891d72cbb7d9d3cc
gzoller <gzoller@gmail.com>
2019-12-07 15:10:44 -0600

    fix null test

--- core/src/test/scala/co.blocke.scalajack/json/misc/PlugHoles.scala
+++ core/src/test/scala/co.blocke.scalajack/json/misc/PlugHoles.scala
@@ -8,10 +8,9 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.funspec.AnyFunSpec
 import util.TypeTags
 
-case class Bogus(
-    num:      Int,
-    unneeded: Option[Boolean],
-    t:        Option[(Int, Boolean)] = Some((5, true)))
+case class Bogus(num: Int,
+                 unneeded: Option[Boolean],
+                 t: Option[(Int, Boolean)] = Some((5, true)))
 
 trait TypeTrait {
   val thing: String
@@ -30,129 +29,129 @@ class PlugHoles() extends AnyFunSpec with Matchers {
   describe(
     "----------------------------\n:  Plug Holes in Coverage  :\n----------------------------"
   ) {
-      it("Tokenizatino") {
-        val js =
-          """{ "name"  :   "Fred",
+    it("Tokenizatino") {
+      val js =
+        """{ "name"  :   "Fred",
           |"age" : 12}""".stripMargin
-        assertResult(Map("name" -> "Fred", "age" -> 12)) {
-          sj.read[Map[String, Any]](js)
-        }
+      assertResult(Map("name" -> "Fred", "age" -> 12)) {
+        sj.read[Map[String, Any]](js)
+      }
 
-        val js2 =
-          """{"thing":"blather on about some stupid subject that no one really cares about"n}"""
-        val msg =
-          """Expected comma here
+      val js2 =
+        """{"thing":"blather on about some stupid subject that no one really cares about"n}"""
+      val msg =
+        """Expected comma here
           |...me stupid subject that no one really cares about"n}
           |----------------------------------------------------^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[Map[String, String]](js2) should have message msg
+      the[ScalaJackError] thrownBy sj.read[Map[String, String]](js2) should have message msg
 
-        val js3 = """{"thing":"blather"n}"""
-        val msg2 =
-          """Expected comma here
+      val js3 = """{"thing":"blather"n}"""
+      val msg2 =
+        """Expected comma here
           |{"thing":"blather"n}
           |------------------^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[Map[String, String]](js3) should have message msg2
-      }
-      it("Missing end of list bracket") {
-        val js = """[12,5"""
-        val msg =
-          """Expected end of list here
+      the[ScalaJackError] thrownBy sj.read[Map[String, String]](js3) should have message msg2
+    }
+    it("Missing end of list bracket") {
+      val js = """[12,5"""
+      val msg =
+        """Expected end of list here
           |[12,5
           |-----^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[List[Int]](js) should have message msg
-      }
-      it("Expected colon in Map") {
-        val js = """{"a",true}"""
-        val msg =
-          """Expected colon here
+      the[ScalaJackError] thrownBy sj.read[List[Int]](js) should have message msg
+    }
+    it("Expected colon in Map") {
+      val js = """{"a",true}"""
+      val msg =
+        """Expected colon here
           |{"a",true}
           |----^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[Map[String, Boolean]](js) should have message msg
-      }
-      it("Missing ending brace in Map") {
-        val js = """{"a":true"""
-        val msg =
-          """Expected end of object here
+      the[ScalaJackError] thrownBy sj.read[Map[String, Boolean]](js) should have message msg
+    }
+    it("Missing ending brace in Map") {
+      val js = """{"a":true"""
+      val msg =
+        """Expected end of object here
           |{"a":true
           |---------^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[Map[String, Boolean]](js) should have message msg
-      }
-      it("Missing ending brace in Object") {
-        val js = """{"name":"Mike","age":35"""
-        val msg =
-          """Expected end of object here
+      the[ScalaJackError] thrownBy sj.read[Map[String, Boolean]](js) should have message msg
+    }
+    it("Missing ending brace in Object") {
+      val js = """{"name":"Mike","age":35"""
+      val msg =
+        """Expected end of object here
           |{"name":"Mike","age":35
           |-----------------------^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[Person](js) should have message msg
-      }
-      it("Missing : on hint scan") {
-        val js =
-          """{"name","_hint":"co.blocke.scalajack.json.misc.Dog","notneeded":[[1,2],[3,4]],"kind":15}"""
-        val msg =
-          """Expected ':' here
+      the[ScalaJackError] thrownBy sj.read[Person](js) should have message msg
+    }
+    it("Missing : on hint scan") {
+      val js =
+        """{"name","_hint":"co.blocke.scalajack.json.misc.Dog","notneeded":[[1,2],[3,4]],"kind":15}"""
+      val msg =
+        """Expected ':' here
           |{"name","_hint":"co.blocke.scalajack.json.misc.Dog","notneeded":[[1,2],[3,4]]...
           |-------^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[Pet](js) should have message msg
-      }
-      it("Unknown character on hint scan") {
-        val js =
-          """{"name":"Fido"Z,"_hint":"co.blocke.scalajack.json.misc.Dog","notneeded":[[1,2],[3,4]],"kind":15}"""
-        val msg =
-          """Unexpected character found
+      the[ScalaJackError] thrownBy sj.read[Pet](js) should have message msg
+    }
+    it("Unknown character on hint scan") {
+      val js =
+        """{"name":"Fido"Z,"_hint":"co.blocke.scalajack.json.misc.Dog","notneeded":[[1,2],[3,4]],"kind":15}"""
+      val msg =
+        """Unexpected character found
           |{"name":"Fido"Z,"_hint":"co.blocke.scalajack.json.misc.Dog","notneeded":[[1,2...
           |--------------^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[Pet](js) should have message msg
-      }
-      it("Missing : while resolving type members") {
-        val js =
-          """{"kind""co.blocke.scalajack.json.misc.Dog","payload":{"name":"Fido","kind":15}}"""
-        val msg =
-          """Expected ':' here
+      the[ScalaJackError] thrownBy sj.read[Pet](js) should have message msg
+    }
+    it("Missing : while resolving type members") {
+      val js =
+        """{"kind""co.blocke.scalajack.json.misc.Dog","payload":{"name":"Fido","kind":15}}"""
+      val msg =
+        """Expected ':' here
             |{"kind""co.blocke.scalajack.json.misc.Dog","payload":{"name":"Fido","kind":15}}
             |-------^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[PetHolder[Pet]](js) should have message msg
-      }
-      it("Unknown character on type member resolution") {
-        val js =
-          """{"kind":"co.blocke.scalajack.json.misc.Dog"Z,"payload":{"name":"Fido","kind":15}}"""
-        val msg =
-          """Unexpected character found
+      the[ScalaJackError] thrownBy sj.read[PetHolder[Pet]](js) should have message msg
+    }
+    it("Unknown character on type member resolution") {
+      val js =
+        """{"kind":"co.blocke.scalajack.json.misc.Dog"Z,"payload":{"name":"Fido","kind":15}}"""
+      val msg =
+        """Unexpected character found
           |{"kind":"co.blocke.scalajack.json.misc.Dog"Z,"payload":{"name":"Fido","kind":...
           |-------------------------------------------^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[PetHolder[Pet]](js) should have message msg
-      }
-      it("Long json error") {
-        val js =
-          """["a""In the dark night when the wolves roam wild did the little rabbit dispair of life itself."]"""
-        val msg =
-          """Expected comma here
+      the[ScalaJackError] thrownBy sj.read[PetHolder[Pet]](js) should have message msg
+    }
+    it("Long json error") {
+      val js =
+        """["a""In the dark night when the wolves roam wild did the little rabbit dispair of life itself."]"""
+      val msg =
+        """Expected comma here
                   |["a""In the dark night when the wolves roam wild did the little rabbit dispai...
                   |----^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[List[String]](js) should have message msg
-      }
-      it("ScalaJack") {
-        val sj = ScalaJack.apply(json.JsonFlavor())
-        sj.read[Int]("15") should be(15)
-      }
-      it("StringBuilder") {
-        val s = StringBuilder()
-        s += "Greg"
-        s.clear()
-        s.result() should be("")
-      }
-      it("End of TA chain") {
-        the[IllegalArgumentException] thrownBy model.DefaultTypeAdapterFactory
-          .typeAdapterOf[Any](null)(sj.taCache, TypeTags.of(typeOf[Any])) should have message """Unable to find a type adapter for Any (may be abstract or a dependency of an abstract class)"""
-      }
-      it("Object reading (json)") {
-        val js = """{5:5}"""
-        val msg =
-          """Expected a String here
+      the[ScalaJackError] thrownBy sj.read[List[String]](js) should have message msg
+    }
+    it("ScalaJack") {
+      val sj = ScalaJack.apply(json.JsonFlavor())
+      sj.read[Int]("15") should be(15)
+    }
+    it("StringBuilder") {
+      val s = StringBuilder()
+      s += "Greg"
+      s.clear()
+      s.result() should be("")
+    }
+    it("End of TA chain") {
+      the[IllegalArgumentException] thrownBy model.DefaultTypeAdapterFactory
+        .typeAdapterOf[Any](null)(sj.taCache, TypeTags.of(typeOf[Any])) should have message """Unable to find a type adapter for Any (may be abstract or a dependency of an abstract class)"""
+    }
+    it("Object reading (json)") {
+      val js = """{5:5}"""
+      val msg =
+        """Expected a String here
           |{5:5}
           |-^""".stripMargin
-        the[ScalaJackError] thrownBy sj.read[Bogus](js) should have message msg
-      }
+      the[ScalaJackError] thrownBy sj.read[Bogus](js) should have message msg
     }
+  }
   it("JsonWriter") {
     val thing: List[BigInt] = List(BigInt(1), null, BigInt(2))
     assertResult("""[1,null,2]""") { sj.render(thing) }
@@ -181,8 +180,11 @@ class PlugHoles() extends AnyFunSpec with Matchers {
                  |^""".stripMargin
     the[ScalaJackError] thrownBy sj.read[Map[String, Int]](jsNotAnArray) should have message msg2
     val badNumber = "12.34.56"
-    the[java.lang.NumberFormatException] thrownBy sj.read[BigDecimal](badNumber) should have message "Character array contains more than one decimal point."
-    the[java.lang.NumberFormatException] thrownBy sj.read[BigInt](badNumber) should have message "For input string: \"12.34.56\""
+    the[java.lang.NumberFormatException] thrownBy sj
+      .read[scala.math.BigDecimal](badNumber) should have message "Character array contains more than one decimal point."
+    the[java.lang.NumberFormatException] thrownBy sj.read[scala.math.BigInt](
+      badNumber
+    ) should have message "For input string: \"12.34.56\""
     assertResult(null) { sj.read[Bogus](jsNull) }
     val msg5 = """Expected start of object here
                  |"Fred"
