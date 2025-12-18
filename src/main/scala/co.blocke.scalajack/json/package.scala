8606809d71104ebb37619a6395e7f595eb3ede80
Greg Zoller <gzoller@gmail.com>
2025-12-18 13:40:58 -0600

    added syntax

--- src/main/scala/co.blocke.scalajack/json/package.scala
+++ src/main/scala/co.blocke.scalajack/json/package.scala
@@ -3,7 +3,6 @@ package json
 
 import scala.quoted.{Expr, Quotes, Type}
 import co.blocke.scala_reflection.reflect.rtypeRefs.*
-import co.blocke.scala_reflection.RTypeRef
 
 val BUFFER_EXCEEDED: Char = 7 // Old "BELL" ASCII value, used as a marker when we've run off the end of the known world
 val END_OF_STRING: Char = 3
